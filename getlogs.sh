b25c25b43644aec89cc62227dfbf36726a9cb426
mxdhi <126177983+mxdhi@users.noreply.github.com>
2023-06-07 19:14:04 +0200

    Update getlogs.sh

--- getlogs.sh
+++ getlogs.sh
@@ -1,39 +1,53 @@
-#!/bin/sh
+#!/bin/bash
 
-# Solicitar número de ticket
-echo "Introduce el número de ticket:"
-read ticket
+# Pregunta por la dirección IP o nombre de host de la máquina virtual
+read -p "Ingrese la dirección IP o el nombre de host de la máquina virtual: " vm_address
 
-APLI="XXXXXXXXXXX"
+# Pregunta por la ruta de los logs en la máquina virtual
+read -p "Ingrese la ruta de los logs en la máquina virtual: " logs_path
 
-# Crear carpeta con el número de ticket y nodos
+# Pregunta por el número de ticket
+read -p "Ingrese el número de ticket: " ticket_number
 
+# Crea la carpeta Logs-TICKET si no existe
+logs_folder="Logs-$ticket_number"
 
-ruta_log="/opt/$APLI/logs/"
-dir_ticket="$ruta_log/$ticket/"
-dir_nodo1="$ruta_log/$ticket/nodo1"
-dir_nodo2="$ruta_log/$ticket/nodo2"
+# Conexión SSH a la máquina virtual y obtención de los logs
+ssh "$vm_address" "ls -lth \"$logs_path\"" | awk 'BEGIN{ count=1 } NR>1 { print count++ ". " $0 }' | head -n 20 > logs_list.txt
 
-mkdir -p "$dir_nodo1"
-mkdir -p "$dir_nodo2"
+# Función para mostrar el listado de logs
+function mostrar_listado_logs() {
+  clear
+  echo "Listado de logs (Últimos 20 logs...):"
+  echo "-----------------"
+  cat logs_list.txt
+  echo "-----------------"
+  echo "1. Descargar log(s)"
+  echo "2. Salir"
+  echo
 
-# Solicitar fecha
-echo "Introduce la fecha (formato YYYY-MM-DD):"
-read fecha
+  read -p "Ingrese una opción: " option
+  case $option in
+    1)
+      read -p "Ingrese el número(s) del log que desea descargar (separado por comas): " log_numbers
+      IFS=',' read -ra nums <<< "$log_numbers"
+      mkdir -p "$logs_folder"
+      for num in "${nums[@]}"; do
+        log_file=$(sed "${num}q;d" logs_list.txt | awk '{print $NF}')
+        scp -q "$vm_address:\"$logs_path/$log_file\"" "$logs_folder/"
+        echo "Log \"$log_file\" descargado en la carpeta \"$PWD/$logs_folder/\"."
+      done
+      ;;
+    2)
+      exit
+      ;;
+    *)
+      echo "Opción inválida. Intente nuevamente."
+      ;;
+  esac
+}
 
-# Obtener la fecha de hoy
-if [ "$fecha" = "$(date +%Y-%m-%d)" ]; then
-  # Si la fecha es la de hoy, copiar archivos sin modificar el nombre
-  echo "Extrayendo logs de hoy..."
-  scp root@*SERVIDOR*:$ruta_log/ARCHIVO.log $dir_nodo1/
-  scp root@*SERVIDOR*:$ruta_log/ARCHIVO.log $dir_nodo2/
-else
-# Si la fecha es anterior:
-  echo "Extrayendo logs de la fecha $fecha..."
-  scp root@*SERVIDOR*:$ruta_log/ARCHIVO_"$fecha"*.log $dir_nodo1/
-  scp root@*SERVIDOR*:$ruta_log/ARCHIVO_"$fecha"*.log $dir_nodo2/
+# Mostrar listado de logs
+mostrar_listado_logs
 
-fi
-
-# Mensaje de finalización
-echo "Logs extraídos y guardados en la carpeta del ticket $dir_ticket"
+rm -Rf logs_list.txt
